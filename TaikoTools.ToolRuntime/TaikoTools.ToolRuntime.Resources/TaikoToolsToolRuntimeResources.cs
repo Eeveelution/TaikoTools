404588f37afed4423aa777fbda53ca7946f48309
Eevee <developerbrzoza@gmail.com>
2021-08-29 00:31:20 +0200

    removed the entire toolruntime

--- TaikoTools.ToolRuntime/TaikoTools.ToolRuntime.Resources/TaikoToolsToolRuntimeResources.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Reflection;
-
-namespace TaikoToolsToolRuntime.Resources {
-    public static class TaikoToolsToolRuntimeResources {
-        public static Assembly ResourceAssembly => typeof(TaikoToolsToolRuntimeResources).Assembly;
-    }
-}
