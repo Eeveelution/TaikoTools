83e6013306f7b06ca560c76931313611d952c1ba
Eevee <developerbrzoza@gmail.com>
2021-08-29 00:34:37 +0200

    removed like all the fuckin code

--- TaikoTools.Components.FrameTimeline/FrameTimeline.cs
+++ TaikoTools.Components.FrameTimeline/FrameTimeline.cs
@@ -1,49 +1,5 @@
-﻿using System.Collections.Generic;
-using osu.Framework.Allocation;
-using osu.Framework.Graphics;
-using osu.Framework.Graphics.Containers;
-using osu.Framework.Graphics.Shapes;
-using osu.Framework.Input.Events;
-using osuTK;
-using osuTK.Graphics;
-using TaikoTools.ReplayParser;
-
+﻿
 namespace TaikoTools.Components.FrameTimeline {
-    public class FrameTimeline : CompositeDrawable {
-        public override bool HandlePositionalInput { get; } = true;
-
-        private Container _frameContainer;
-
-        public Vector2 TargetSize;
-        public Vector2 WindowSize;
-
-        public FrameTimeline(Vector2 windowSize, Vector2 targetSize, List<ReplayClick> replayClicks) {
-            this.TargetSize = targetSize;
-            this.WindowSize = windowSize;
-        }
-
-        [BackgroundDependencyLoader]
-        private void load() {
-            this.Size     = this.TargetSize;
-            this.Position = new Vector2(0, this.WindowSize.Y - this.TargetSize.Y);
-
-            this.InternalChild = this._frameContainer = new Container {
-                Anchor   = Anchor.TopLeft,
-                Size     = this.TargetSize,
-                Origin   = Anchor.TopLeft,
-                Children = new [] {
-                    new Box() {
-                        Size     = this.TargetSize,
-                        Colour   = Color4.Red,
-
-                    },
-                },
-            };
-        }
-
-        protected override bool OnClick(ClickEvent e) {
-            base.OnClick(e);
-            return true;
-        }
+    public class FrameTimeline {
     }
 }
--- TaikoTools.Tool.ReplayViewer/ReplayViewerScreen.cs
+++ TaikoTools.Tool.ReplayViewer/ReplayViewerScreen.cs
@@ -1,37 +1,6 @@
-using System.Collections.Generic;
-using osu.Framework.Allocation;
-using osu.Framework.Graphics;
-using osu.Framework.Graphics.Shapes;
-using osu.Framework.Graphics.Sprites;
-using osu.Framework.Screens;
-using osuTK;
-using osuTK.Graphics;
-using TaikoTools.Components.FrameTimeline;
-using TaikoTools.ReplayParser;
 
 namespace TaikoTools.Tool.ReplayViewer {
-    public class ReplayViewerScreen : Screen {
-        [BackgroundDependencyLoader]
-        private void load() {
-            InternalChildren = new Drawable[] {
-                //soon to be background
-                new Box {
-                    Colour = Color4.Violet, RelativeSizeAxes = Axes.Both,
-                },
-                new Box {
-                    Colour = Color4.DimGray,
-                    Position = new Vector2(0, 0),
-                    Size = new Vector2(1366, 48)
-                },
-                new SpriteText {
-                    Y      = 20,
-                    Text   = "Taiko Replay Viewer",
-                    Anchor = Anchor.TopCentre,
-                    Origin = Anchor.TopCentre,
-                    Font   = FontUsage.Default.With(size: 40)
-                },
-                new FrameTimeline( new Vector2(1366, 768), new Vector2(1366, 288), new List<ReplayClick>())
-            };
-        }
+    public class ReplayViewerScreen {
+
     }
 }
