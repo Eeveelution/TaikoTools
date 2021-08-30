ee3cab8f95ccdf8a3ec5197ed2f09d79e8a5855d
Eevee <developerbrzoza@gmail.com>
2021-08-30 23:46:03 +0200

    getting ready to do some drawing

--- TaikoTools.Components.FrameTimeline/Drawables/DrawableFrame.cs
+++ TaikoTools.Components.FrameTimeline/Drawables/DrawableFrame.cs
@@ -1,7 +1,11 @@
+using PeppyCodeEngineGL.Engine.Graphics.Sprites;
 using TaikoTools.ReplayParser;
 
 namespace TaikoTools.Components.FrameTimeline {
     public class DrawableFrame {
+        private pSprite _frameSprite;
+
+        public int Time;
         public DrawableFrame(FrameTimeline timeline, ReplayClick click) {
 
         }
--- TaikoTools.Components.FrameTimeline/FrameTimeline.cs
+++ TaikoTools.Components.FrameTimeline/FrameTimeline.cs
@@ -1,11 +1,30 @@
 ﻿
+using System;
+using System.Collections.Generic;
 using Microsoft.Xna.Framework.Graphics;
+using PeppyCodeEngineGL.Engine.Audio;
 using PeppyCodeEngineGL.Engine.Graphics.Sprites;
 
 namespace TaikoTools.Components.FrameTimeline {
     public class FrameTimeline : pDrawable {
+        public double TimelineRange = 2000.0;
+
+        private List<DrawableFrame> _drawableFrames = new();
+
+        public double TimeToTimelinePos(double currentTime, double time) {
+            double timelineLeftBound = currentTime  - this.TimelineRange / 2.0;
+            double timelineRightBound = currentTime + this.TimelineRange / 2.0;
+
+            return Math.Min(Math.Max((0.0 + ((this.TimelineRange - timelineLeftBound) / this.TimelineRange) * this.TimelineRange / 2.0), 0.0), 0.0 + this.TimelineRange / 2.0);
+        }
+
         public override void Draw(SpriteBatch batch, SpriteManagerArgs args) {
-            throw new System.NotImplementedException();
+            double minTime = AudioController.Time - this.TimelineRange / 2.0;
+            double maxTime = AudioController.Time + this.TimelineRange / 2.0;
+
+            if (AudioController.Time < 1500) {
+                minTime = 0;
+            }
         }
     }
 }
